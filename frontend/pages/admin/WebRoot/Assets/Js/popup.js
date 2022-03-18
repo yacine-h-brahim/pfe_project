329aaf4ada72986d3a72e9c6d765d0b26880bf68
Chetoui <hossem.chetoui@gmail.com>
2022-03-18 01:03:35 +0100

    added popup show and hide for validate theme page

---
+++ frontend/pages/admin/WebRoot/Assets/Js/popup.js
@@ -0,0 +1,25 @@
+
+
+OnLoad(() => {
+    // Get the popup page
+    var popup = document.querySelector(".popup-container");
+
+    // Get the button that will open up the popup
+    var openButton = document.querySelector(".butto");
+
+    // Listen for it's click events
+    openButton.addEventListener("click", () => {
+        // Make the popup appear when clicked
+        popup.classList.add("appear");
+    });
+
+    // Get the button that will close the popup
+    var closeButton = document.querySelector(".close");
+
+    // Listen out for click events
+    closeButton.addEventListener("click", () => {
+        // Make the popup disappear when clicked
+        popup.classList.remove("appear");
+    });
+
+});
