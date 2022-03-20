466295da31fe57c32b1e12ad1b3c5af654e231db
Chetoui <hossem.chetoui@gmail.com>
2022-03-20 08:52:35 +0100

    test

--- frontend/pages/admin/WebRoot/Assets/Js/popup.js
+++ frontend/pages/admin/WebRoot/Assets/Js/popup.js
@@ -1,25 +1,29 @@
 
 
 OnLoad(() => {
+
+
     // Get the popup page
     var popup = document.querySelector(".popup-container");
 
-    // Get the button that will open up the popup
-    var openButton = document.querySelector(".butto");
+    popup.classList.add("appear");
 
-    // Listen for it's click events
-    openButton.addEventListener("click", () => {
-        // Make the popup appear when clicked
-        popup.classList.add("appear");
-    });
+    // // Get the button that will open up the popup
+    // var openButton = document.querySelector(".butto");
 
-    // Get the button that will close the popup
-    var closeButton = document.querySelector(".close");
+    // // Listen for it's click events
+    // openButton.addEventListener("click", () => {
+    //     // Make the popup appear when clicked
+    //     popup.classList.add("appear");
+    // });
 
-    // Listen out for click events
-    closeButton.addEventListener("click", () => {
-        // Make the popup disappear when clicked
-        popup.classList.remove("appear");
-    });
+    // // Get the button that will close the popup
+    // var closeButton = document.querySelector(".close");
+
+    // // Listen out for click events
+    // closeButton.addEventListener("click", () => {
+    //     // Make the popup disappear when clicked
+    //     popup.classList.remove("appear");
+    // });
 
 });
