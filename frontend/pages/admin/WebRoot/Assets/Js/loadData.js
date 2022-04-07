43ffe991ea9bc68ed773b15c6f23a955bfcef6a7
Chetoui <hossem.chetoui@gmail.com>
2022-04-07 18:00:51 +0100

    finished the popup for theme validation

--- frontend/pages/admin/WebRoot/Assets/Js/loadData.js
+++ frontend/pages/admin/WebRoot/Assets/Js/loadData.js
@@ -7,53 +7,249 @@ OnLoad(() => {
 
     // Get the data from the server 
     // TODO: remove the sample data and get it from the server
-    var data = [{
+    // make this async
+    window.data = [{
         id: "1",
         title: "gestion defa(fdapoidda(p iapdfadfdafjdalf df afjdfmlajdf mldj laj flmadjk fldsj flkjsd lkjdsaml fjamlfkj mdsalj flmksdj flkaj)) pfe",
-        description: "on veut cree une application full web qui va nous aider a faire la gestion des pfes",
+        description: "Le but de ce projet est de concevoir et réaliser une application pour la gestion des projets de fin d’étude au sein de l’ESI de Sidi Bel Abbes."
+            + "\r\n" + "Cette application s’articule sur trois sessions principales :"
+            + "\r\n" + "Session Enseignant :"
+            + "\r\n" + "-Dépôt des P.F.E,"
+            + "\r\n" + "-suivi et évaluation de l’équipe de projet,"
+            + "\r\n" + "-échange avec les étudiants sur la plateforme,"
+            + "\r\n" + "-Dépôt de l’autorisation de soutenance."
+            + "\r\n" + "Session Etudiant :"
+            + "\r\n" + "-Composition et validation de l’équipe de projet,"
+            + "\r\n" + "-Soumission de la fiche de vœux pour le choix de sujet,"
+            + "\r\n" + "-Dépôt de livrables."
+            + "\r\n" + "Session administrateur "
+            + "\r\n" + "-Gestion des comptes et droits d’accès",
         encadreur: "mohammed, ben snane et ahmed",
+        niveau: "1cs tc",
+        motsCles: "web client-server",
+        frontend: "Vue.JS, Express, HTML5, CSS3, JavaScript (ou autres langages dédiés)",
+        backend: " NodeJs (ou autres langages dédiés)",
+        planDeTravail: "Analyse et spécification des besoins."
+            + "\r\n" + "Conception"
+            + "\r\n" + "Implémentation"
+            + "\r\n" + "Vérification et validation"
+            + "\r\n" + "Rédaction du Rapport "
     },
     {
         id: "2",
-        title: "gestion de pfe",
-        description: "on veut cree une application full web qui va nous aider a faire la gestion des pfes",
+        title: "gestion defa(fdapoidda(p iapdfadfdafjdalf df afjdfmlajdf mldj laj flmadjk fldsj flkjsd lkjdsaml fjamlfkj mdsalj flmksdj flkaj)) pfe",
+        description: "Le but de ce projet est de concevoir et réaliser une application pour la gestion des projets de fin d’étude au sein de l’ESI de Sidi Bel Abbes."
+            + "\r\n" + "Cette application s’articule sur trois sessions principales :"
+            + "\r\n" + "Session Enseignant :"
+            + "\r\n" + "-Dépôt des P.F.E,"
+            + "\r\n" + "-suivi et évaluation de l’équipe de projet,"
+            + "\r\n" + "-échange avec les étudiants sur la plateforme,"
+            + "\r\n" + "-Dépôt de l’autorisation de soutenance."
+            + "\r\n" + "Session Etudiant :"
+            + "\r\n" + "-Composition et validation de l’équipe de projet,"
+            + "\r\n" + "-Soumission de la fiche de vœux pour le choix de sujet,"
+            + "\r\n" + "-Dépôt de livrables."
+            + "\r\n" + "Session administrateur "
+            + "\r\n" + "-Gestion des comptes et droits d’accès",
         encadreur: "mohammed, ben snane et ahmed",
+        niveau: "1cs tc",
+        motsCles: "web client-server",
+        frontend: "Vue.JS, Express, HTML5, CSS3, JavaScript (ou autres langages dédiés)",
+        backend: " NodeJs (ou autres langages dédiés)",
+        planDeTravail: "Analyse et spécification des besoins."
+            + "\r\n" + "Conception"
+            + "\r\n" + "Implémentation"
+            + "\r\n" + "Vérification et validation"
+            + "\r\n" + "Rédaction du Rapport "
     },
     {
         id: "3",
-        title: "gestion de pfe",
-        description: "on veut cree une application full web qui va nous aider a faire la gestion des pfes",
+        title: "gestion defa(fdapoidda(p iapdfadfdafjdalf df afjdfmlajdf mldj laj flmadjk fldsj flkjsd lkjdsaml fjamlfkj mdsalj flmksdj flkaj)) pfe",
+        description: "Le but de ce projet est de concevoir et réaliser une application pour la gestion des projets de fin d’étude au sein de l’ESI de Sidi Bel Abbes."
+            + "\r\n" + "Cette application s’articule sur trois sessions principales :"
+            + "\r\n" + "Session Enseignant :"
+            + "\r\n" + "-Dépôt des P.F.E,"
+            + "\r\n" + "-suivi et évaluation de l’équipe de projet,"
+            + "\r\n" + "-échange avec les étudiants sur la plateforme,"
+            + "\r\n" + "-Dépôt de l’autorisation de soutenance."
+            + "\r\n" + "Session Etudiant :"
+            + "\r\n" + "-Composition et validation de l’équipe de projet,"
+            + "\r\n" + "-Soumission de la fiche de vœux pour le choix de sujet,"
+            + "\r\n" + "-Dépôt de livrables."
+            + "\r\n" + "Session administrateur "
+            + "\r\n" + "-Gestion des comptes et droits d’accès",
         encadreur: "mohammed, ben snane et ahmed",
+        niveau: "1cs tc",
+        motsCles: "web client-server",
+        frontend: "Vue.JS, Express, HTML5, CSS3, JavaScript (ou autres langages dédiés)",
+        backend: " NodeJs (ou autres langages dédiés)",
+        planDeTravail: "Analyse et spécification des besoins."
+            + "\r\n" + "Conception"
+            + "\r\n" + "Implémentation"
+            + "\r\n" + "Vérification et validation"
+            + "\r\n" + "Rédaction du Rapport "
     },
     {
         id: "4",
-        title: "gestion de pfe",
-        description: "on veut cree une application full web qui va nous aider a faire la gestion des pfes",
+        title: "gestion defa(fdapoidda(p iapdfadfdafjdalf df afjdfmlajdf mldj laj flmadjk fldsj flkjsd lkjdsaml fjamlfkj mdsalj flmksdj flkaj)) pfe",
+        description: "Le but de ce projet est de concevoir et réaliser une application pour la gestion des projets de fin d’étude au sein de l’ESI de Sidi Bel Abbes."
+            + "\r\n" + "Cette application s’articule sur trois sessions principales :"
+            + "\r\n" + "Session Enseignant :"
+            + "\r\n" + "-Dépôt des P.F.E,"
+            + "\r\n" + "-suivi et évaluation de l’équipe de projet,"
+            + "\r\n" + "-échange avec les étudiants sur la plateforme,"
+            + "\r\n" + "-Dépôt de l’autorisation de soutenance."
+            + "\r\n" + "Session Etudiant :"
+            + "\r\n" + "-Composition et validation de l’équipe de projet,"
+            + "\r\n" + "-Soumission de la fiche de vœux pour le choix de sujet,"
+            + "\r\n" + "-Dépôt de livrables."
+            + "\r\n" + "Session administrateur "
+            + "\r\n" + "-Gestion des comptes et droits d’accès",
         encadreur: "mohammed, ben snane et ahmed",
+        niveau: "1cs tc",
+        motsCles: "web client-server",
+        frontend: "Vue.JS, Express, HTML5, CSS3, JavaScript (ou autres langages dédiés)",
+        backend: " NodeJs (ou autres langages dédiés)",
+        planDeTravail: "Analyse et spécification des besoins."
+            + "\r\n" + "Conception"
+            + "\r\n" + "Implémentation"
+            + "\r\n" + "Vérification et validation"
+            + "\r\n" + "Rédaction du Rapport "
     },
     {
         id: "5",
-        title: "gestion de pfe",
-        description: "on veut cree une application full web qui va nous aider a faire la gestion des pfes",
+        title: "gestion defa(fdapoidda(p iapdfadfdafjdalf df afjdfmlajdf mldj laj flmadjk fldsj flkjsd lkjdsaml fjamlfkj mdsalj flmksdj flkaj)) pfe",
+        description: "Le but de ce projet est de concevoir et réaliser une application pour la gestion des projets de fin d’étude au sein de l’ESI de Sidi Bel Abbes."
+            + "\r\n" + "Cette application s’articule sur trois sessions principales :"
+            + "\r\n" + "Session Enseignant :"
+            + "\r\n" + "-Dépôt des P.F.E,"
+            + "\r\n" + "-suivi et évaluation de l’équipe de projet,"
+            + "\r\n" + "-échange avec les étudiants sur la plateforme,"
+            + "\r\n" + "-Dépôt de l’autorisation de soutenance."
+            + "\r\n" + "Session Etudiant :"
+            + "\r\n" + "-Composition et validation de l’équipe de projet,"
+            + "\r\n" + "-Soumission de la fiche de vœux pour le choix de sujet,"
+            + "\r\n" + "-Dépôt de livrables."
+            + "\r\n" + "Session administrateur "
+            + "\r\n" + "-Gestion des comptes et droits d’accès",
         encadreur: "mohammed, ben snane et ahmed",
+        niveau: "1cs tc",
+        motsCles: "web client-server",
+        frontend: "Vue.JS, Express, HTML5, CSS3, JavaScript (ou autres langages dédiés)",
+        backend: " NodeJs (ou autres langages dédiés)",
+        planDeTravail: "Analyse et spécification des besoins."
+            + "\r\n" + "Conception"
+            + "\r\n" + "Implémentation"
+            + "\r\n" + "Vérification et validation"
+            + "\r\n" + "Rédaction du Rapport "
     },
     {
         id: "6",
-        title: "gestion de pfe",
-        description: "on veut cree une application full web qui va nous aider a faire la gestion des pfes",
+        title: "gestion defa(fdapoidda(p iapdfadfdafjdalf df afjdfmlajdf mldj laj flmadjk fldsj flkjsd lkjdsaml fjamlfkj mdsalj flmksdj flkaj)) pfe",
+        description: "Le but de ce projet est de concevoir et réaliser une application pour la gestion des projets de fin d’étude au sein de l’ESI de Sidi Bel Abbes."
+            + "\r\n" + "Cette application s’articule sur trois sessions principales :"
+            + "\r\n" + "Session Enseignant :"
+            + "\r\n" + "-Dépôt des P.F.E,"
+            + "\r\n" + "-suivi et évaluation de l’équipe de projet,"
+            + "\r\n" + "-échange avec les étudiants sur la plateforme,"
+            + "\r\n" + "-Dépôt de l’autorisation de soutenance."
+            + "\r\n" + "Session Etudiant :"
+            + "\r\n" + "-Composition et validation de l’équipe de projet,"
+            + "\r\n" + "-Soumission de la fiche de vœux pour le choix de sujet,"
+            + "\r\n" + "-Dépôt de livrables."
+            + "\r\n" + "Session administrateur "
+            + "\r\n" + "-Gestion des comptes et droits d’accès",
         encadreur: "mohammed, ben snane et ahmed",
+        niveau: "1cs tc",
+        motsCles: "web client-server",
+        frontend: "Vue.JS, Express, HTML5, CSS3, JavaScript (ou autres langages dédiés)",
+        backend: " NodeJs (ou autres langages dédiés)",
+        planDeTravail: "Analyse et spécification des besoins."
+            + "\r\n" + "Conception"
+            + "\r\n" + "Implémentation"
+            + "\r\n" + "Vérification et validation"
+            + "\r\n" + "Rédaction du Rapport "
     },
     {
         id: "7",
-        title: "gestion de pfe",
-        description: "on veut cree une application full web qui va nous aider a faire la gestion des pfes",
+        title: "gestion defa(fdapoidda(p iapdfadfdafjdalf df afjdfmlajdf mldj laj flmadjk fldsj flkjsd lkjdsaml fjamlfkj mdsalj flmksdj flkaj)) pfe",
+        description: "Le but de ce projet est de concevoir et réaliser une application pour la gestion des projets de fin d’étude au sein de l’ESI de Sidi Bel Abbes."
+            + "\r\n" + "Cette application s’articule sur trois sessions principales :"
+            + "\r\n" + "Session Enseignant :"
+            + "\r\n" + "-Dépôt des P.F.E,"
+            + "\r\n" + "-suivi et évaluation de l’équipe de projet,"
+            + "\r\n" + "-échange avec les étudiants sur la plateforme,"
+            + "\r\n" + "-Dépôt de l’autorisation de soutenance."
+            + "\r\n" + "Session Etudiant :"
+            + "\r\n" + "-Composition et validation de l’équipe de projet,"
+            + "\r\n" + "-Soumission de la fiche de vœux pour le choix de sujet,"
+            + "\r\n" + "-Dépôt de livrables."
+            + "\r\n" + "Session administrateur "
+            + "\r\n" + "-Gestion des comptes et droits d’accès",
         encadreur: "mohammed, ben snane et ahmed",
+        niveau: "1cs tc",
+        motsCles: "web client-server",
+        frontend: "Vue.JS, Express, HTML5, CSS3, JavaScript (ou autres langages dédiés)",
+        backend: " NodeJs (ou autres langages dédiés)",
+        planDeTravail: "Analyse et spécification des besoins."
+            + "\r\n" + "Conception"
+            + "\r\n" + "Implémentation"
+            + "\r\n" + "Vérification et validation"
+            + "\r\n" + "Rédaction du Rapport "
     },
     {
         id: "8",
-        title: "gestion de pfe",
-        description: "on veut cree une application full web qui va nous aider a faire la gestion des pfes",
+        title: "gestion defa(fdapoidda(p iapdfadfdafjdalf df afjdfmlajdf mldj laj flmadjk fldsj flkjsd lkjdsaml fjamlfkj mdsalj flmksdj flkaj)) pfe",
+        description: "Le but de ce projet est de concevoir et réaliser une application pour la gestion des projets de fin d’étude au sein de l’ESI de Sidi Bel Abbes."
+            + "\r\n" + "Cette application s’articule sur trois sessions principales :"
+            + "\r\n" + "Session Enseignant :"
+            + "\r\n" + "-Dépôt des P.F.E,"
+            + "\r\n" + "-suivi et évaluation de l’équipe de projet,"
+            + "\r\n" + "-échange avec les étudiants sur la plateforme,"
+            + "\r\n" + "-Dépôt de l’autorisation de soutenance."
+            + "\r\n" + "Session Etudiant :"
+            + "\r\n" + "-Composition et validation de l’équipe de projet,"
+            + "\r\n" + "-Soumission de la fiche de vœux pour le choix de sujet,"
+            + "\r\n" + "-Dépôt de livrables."
+            + "\r\n" + "Session administrateur "
+            + "\r\n" + "-Gestion des comptes et droits d’accès",
         encadreur: "mohammed, ben snane et ahmed",
+        niveau: "1cs tc",
+        motsCles: "web client-server",
+        frontend: "Vue.JS, Express, HTML5, CSS3, JavaScript (ou autres langages dédiés)",
+        backend: " NodeJs (ou autres langages dédiés)",
+        planDeTravail: "Analyse et spécification des besoins."
+            + "\r\n" + "Conception"
+            + "\r\n" + "Implémentation"
+            + "\r\n" + "Vérification et validation"
+            + "\r\n" + "Rédaction du Rapport "
+    },
+    {
+        id: "9",
+        title: "gestion defa(fdapoidda(p iapdfadfdafjdalf df afjdfmlajdf mldj laj flmadjk fldsj flkjsd lkjdsaml fjamlfkj mdsalj flmksdj flkaj)) pfe",
+        description: "Le but de ce projet est de concevoir et réaliser une application pour la gestion des projets de fin d’étude au sein de l’ESI de Sidi Bel Abbes."
+            + "\r\n" + "Cette application s’articule sur trois sessions principales :"
+            + "\r\n" + "Session Enseignant :"
+            + "\r\n" + "-Dépôt des P.F.E,"
+            + "\r\n" + "-suivi et évaluation de l’équipe de projet,"
+            + "\r\n" + "-échange avec les étudiants sur la plateforme,"
+            + "\r\n" + "-Dépôt de l’autorisation de soutenance."
+            + "\r\n" + "Session Etudiant :"
+            + "\r\n" + "-Composition et validation de l’équipe de projet,"
+            + "\r\n" + "-Soumission de la fiche de vœux pour le choix de sujet,"
+            + "\r\n" + "-Dépôt de livrables."
+            + "\r\n" + "Session administrateur "
+            + "\r\n" + "-Gestion des comptes et droits d’accès",
+        encadreur: "mohammed, ben snane et ahmed",
+        niveau: "1cs tc",
+        motsCles: "web client-server",
+        frontend: "Vue.JS, Express, HTML5, CSS3, JavaScript (ou autres langages dédiés)",
+        backend: " NodeJs (ou autres langages dédiés)",
+        planDeTravail: "Analyse et spécification des besoins."
+            + "\r\n" + "Conception"
+            + "\r\n" + "Implémentation"
+            + "\r\n" + "Vérification et validation"
+            + "\r\n" + "Rédaction du Rapport "
     }
     ]
 
@@ -61,7 +257,7 @@ OnLoad(() => {
     // ==========================
 
     // For each item in the list of themes
-    data.forEach(theme => {
+    window.data.forEach(theme => {
         // Create a new row in the table
         var row = table.insertRow();
 
@@ -123,26 +319,25 @@ OnLoad(() => {
         // Add click events to each button
         acceptButton.addEventListener("click", function () { accept(id) });
         rejectButton.addEventListener("click", function () { reject(id) });
-        seeMoreButton.addEventListener("click", function () { seeMore(id, data) });
+        seeMoreButton.addEventListener("click", function () { seeMore(id) });
 
     }
 
 
     // Some styles for the popup page, cuz i couldn't do it in css
+    // Set the minimum height of the popup to take the whole page so no background will be shown if it doesn't
+
     // Get the page
     var page = document.querySelector(".page-container");
 
     // Get the top bar
     var topBar = document.querySelector(".top-bar");
 
-    // Get the popup page
-    var popup = document.querySelector(".popup-container");
-
     // Get the height of the whole page
     const fullHeight = page.clientHeight + topBar.clientHeight;
 
     // Set the minimum height of the popup to be the same as the page
-    popup.style.minHeight = fullHeight + "px";
+    window.popup.style.minHeight = fullHeight;
 
 });
 
@@ -168,17 +363,17 @@ function reject(id) {
 
 // This function will get executed when the see more button gets clicked,
 // it concerns the theme with the specified id
-function seeMore(id, data) {
+function seeMore(id) {
 
     // An index in the loop, it will contain the index of the element with the specific id
     // in the data array
     var index = 0;
 
     // While the element with the id hasn't been found
-    while (index < data.length) {
+    while (index < window.data.length) {
 
         // Check if the element has the wanted id
-        if (data[index].id == id) {
+        if (window.data[index].id == id) {
             // Break the loop if it does    
             break;
         }
@@ -186,7 +381,37 @@ function seeMore(id, data) {
         index++;
     }
 
-    // TODO: Show the element to the user
-    // Add a popup and show the data
-    console.log(data[index]);
+    // Get the form in the popup
+    var form = window.popup.children[0];
+
+    // Set the title
+    form.children[1].children[1].value = window.data[index].title;
+
+    // Set encadreur
+    form.children[2].children[1].value = window.data[index].encadreur;
+
+    // Set level
+    form.children[3].children[1].value = window.data[index].niveau;
+
+    // Set keywords
+    form.children[4].children[1].value = window.data[index].motsCles;
+
+    // Set resume
+    form.children[5].children[1].value = window.data[index].description;
+
+    // Set frontend technologies
+    form.children[6].children[1].value = window.data[index].frontend;
+
+    // Set backend technologies
+    form.children[7].children[1].value = window.data[index].backend;
+
+    // Set plan de travail
+    form.children[8].children[1].value = window.data[index].planDeTravail;
+
+    // Scroll to the top
+    window.scrollTo(0, 0);
+
+    // Make the popup appear
+    window.popup.classList.add("appear");
+
 }
