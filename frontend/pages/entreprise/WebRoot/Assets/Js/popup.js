76633b07f98c849decf9ea352e2c16c16dc7d1e4
NOR <n.bennacer@esi-sba.dz>
2022-04-07 12:59:41 +0100

    refresh

---
+++ frontend/pages/entreprise/WebRoot/Assets/Js/popup.js
@@ -0,0 +1,27 @@
+
+OnLoad(() => {
+    // Get the popup page
+    var popup = document.querySelector(".popup-container");
+
+    // Get the button that will open up the popup
+    var openButton = document.querySelector(".butto");
+
+    // Remove this after finishing making the page
+    popup.classList.add("appear");
+
+    // Listen for it's click events
+    openButton.addEventListener("click", () => {
+        // Make the popup appear when clicked
+        popup.classList.add("appear");
+    });
+
+    // Get the button that will close the popup
+    var closeButton = document.querySelector(".close");
+
+    // Listen out for click events
+    closeButton.addEventListener("click", () => {
+        // Make the popup disappear when clicked
+        popup.classList.remove("appear");
+    });
+
+});
